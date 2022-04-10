52c7c3b784b6393c4543426b693c78db3cbae83f
Rodrigo Cândido <rodrigocan@gmail.com>
2022-04-10 17:35:17 -0300

    Aula 02 - Resolvers & Queries

---
+++ purchases/src/http/graphql/resolvers/purchases.resolver.ts
@@ -0,0 +1,59 @@
+import { UseGuards } from '@nestjs/common';
+import {
+  Args,
+  Mutation,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
+
+import { AuthUser, CurrentUser } from 'src/http/auth/current-user';
+import { CustomersService } from 'src/services/customers.service';
+import { ProductsService } from 'src/services/products.service';
+import { PurchasesService } from 'src/services/purchases.service';
+import { AuthorizationGuard } from '../../auth/authorization.guard';
+import { CreatePurchaseInput } from '../input/create-purchase-input';
+import { Purchase } from '../models/purchase';
+
+@Resolver(() => Purchase)
+export class PurchasesResolver {
+  constructor(
+    private purchasesService: PurchasesService,
+    private productsService: ProductsService,
+    private customersService: CustomersService,
+  ) {}
+
+  @UseGuards(AuthorizationGuard)
+  @Query(() => [Purchase])
+  purchases() {
+    return this.purchasesService.listAllPurchases();
+  }
+
+  @ResolveField()
+  product(@Parent() purchase: Purchase) {
+    return this.productsService.getProductById(purchase.productId);
+  }
+
+  @UseGuards(AuthorizationGuard)
+  @Mutation(() => Purchase)
+  async createPurchase(
+    @Args('data') data: CreatePurchaseInput,
+    @CurrentUser() user: AuthUser,
+  ) {
+    let customer = await this.customersService.getCustomerByAuthUserId(
+      user.sub,
+    );
+
+    if (!customer) {
+      customer = await this.customersService.createCustomer({
+        authUserId: user.sub,
+      });
+    }
+
+    return this.purchasesService.createPurchase({
+      customerId: customer.id,
+      productId: data.productId,
+    });
+  }
+}
--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -5,7 +5,12 @@ import { GraphQLModule } from '@nestjs/graphql';
 import path from 'node:path';
 
 import { DatabaseModule } from '../database/database.module';
-import { TestResolver } from './test.resolver';
+import { ProductsService } from 'src/services/products.service';
+import { ProductsResolver } from './graphql/resolvers/products.resolver';
+import { PurchasesResolver } from './graphql/resolvers/purchases.resolver';
+import { PurchasesService } from 'src/services/purchases.service';
+import { CustomersService } from 'src/services/customers.service';
+import { CustomersResolver } from './graphql/resolvers/customers.resolver';
 
 @Module({
   imports: [
@@ -16,6 +21,16 @@ import { TestResolver } from './test.resolver';
       autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
     }),
   ],
-  providers: [TestResolver],
+  providers: [
+    // Resolvers
+    ProductsResolver,
+    PurchasesResolver,
+    CustomersResolver,
+
+    // Services
+    ProductsService,
+    PurchasesService,
+    CustomersService,
+  ],
 })
 export class HttpModule {}
